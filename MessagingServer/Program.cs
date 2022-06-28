820b8814f7dac64edfebf6db10d2e5ff513c136d
Fili_vw23@student.itstep.org <Fili_vw23@student.itstep.org>
2022-06-28 18:25:18 +0300

    init server

---
+++ MessagingServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MessagingServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
